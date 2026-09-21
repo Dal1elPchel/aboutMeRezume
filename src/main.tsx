73cd443e7f0ddabd75e82e447e124fb6fb8ce51d
daniel <danielpchelincev987@mail.ru>
2026-09-21 20:05:18 +0300

    created header: used new theme context and header

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './app/index.scss'
-import App from './app/App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './app/index.scss';
+import App from './app/App.tsx';
+import { ThemeProvider } from '@/shared/library/theme/ThemeProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
-)
+);
